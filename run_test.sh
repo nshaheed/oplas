11a7c28697626a7f748ea5e322ce6ebd86a2584b
Nick Andrew Shaheed <nshaheed@sh04-ln03.stanford.edu>
2025-09-19 15:21:20 -0700

    added run_test.sh

---
+++ run_test.sh
@@ -0,0 +1,14 @@
+ml load system
+ml load uv
+ml load gcc/14.2.0
+ml load cudnn
+ml load libsndfile
+ml load ffmpeg
+# ml load python/3.12.1
+# ml load py-pytorch/2.4.1_py312
+# ml load py-scipystack
+# ml load py-wandb
+
+nvidia-smi
+
+uv run train_sweep.py --data_dir $SCRATCH/musdb18 --test
